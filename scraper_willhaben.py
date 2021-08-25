f12925b6ef36e592d5d471355cf4c71ce25bc9f3
Patrick S <patrick.stampler@gmail.com>
2021-08-25 18:12:45 +0200

    update

--- scraper_willhaben.py
+++ scraper_willhaben.py
@@ -1,10 +1,13 @@
 from os import error
-from sqlite3.dbapi2 import Connection, Cursor, connect
+from sqlite3.dbapi2 import Connection, Cursor, Date, connect
 from bs4 import BeautifulSoup
 import urllib.request
 import re
 import sqlite3
 import csv
+import time
+from datetime import datetime
+from datetime import date
 
 
 def create_connection(db_file):
@@ -30,34 +33,33 @@ def get_apartment_details(url):
     fp.close()
 
     soup = BeautifulSoup(mystr, 'html.parser')
+    time.sleep(1)
     soupString = str(soup)
 
     post_code = re.search('post_code":"(.*?)"', soupString)
     price = re.search('price":"(.*?)"', soupString)
     rooms = re.search('rooms":"(.*?)"', soupString)
+    timestamp = datetime.now()
+    date = Date.today()
 
-    try:
-        print("This is apartment in " + post_code.group(1) + ", with " +
-              rooms.group(1) + " rooms, price is " + price.group(1) + "." + url)
-    except AttributeError:
-        print("x")
+    print("This is apartment in " + post_code.group(1) + ", with " +
+          rooms.group(1) + " rooms, price is " + price.group(1) + "." + " " + url)
 
-    return post_code.group(1), price.group(1), rooms.group(1)
+    return post_code.group(1), price.group(1), rooms.group(1), timestamp, date
 
 
 graz_apartment_links = []
 
-page_count = 1
-
 for i in range(1, 4):
     fp = urllib.request.urlopen(
-        "https://www.willhaben.at/iad/immobilien/eigentumswohnung/eigentumswohnung-angebote?&rows=100&areaId=601&parent_areaid=6")
+        "https://www.willhaben.at/iad/immobilien/eigentumswohnung/steiermark/graz/?rows=5&page=" + str(i))
 
     mybytes = fp.read()
     mystr = mybytes.decode("ISO-8859-1")
     fp.close()
 
     soup = BeautifulSoup(mystr, 'html.parser')
+    time.sleep(1)
 
     for link in soup.find_all('a'):
         url = link.get('href')
@@ -74,12 +76,16 @@ for i in range(1, 4):
     cursor = db.cursor()
 
     for url in graz_apartment_links:
-        post_code, price, rooms = get_apartment_details(url)
-        combined = post_code + ',' + price + ',' + rooms + ',' + url
-        writer.writerow([combined])
-        count = cursor.execute("INSERT INTO immobuy(zip, price, rooms, url) VALUES(?,?,?,?)", (
-            post_code, price, rooms, url))
-        db.commit()
+        try:
+            post_code, price, rooms, timestamp, date = get_apartment_details(
+                url)
+            combined = post_code + ',' + price + ',' + rooms + ',' + url
+            writer.writerow([combined])
+            count = cursor.execute("INSERT INTO immobuy(zip, price, rooms, url, timestamp, date) VALUES(?,?,?,?,?,?)", (
+                post_code, price, rooms, url, timestamp, date))
+            db.commit()
+        except:
+            continue
 
 # close file
 f.close()
